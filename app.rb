f22fd1df72d4ffeaad1f1c9d5ec1d3c9679b133f
Erin Anderson <e.anderson1226@gmail.com>
2021-03-17 14:49:20 -0400

    commit tree 4c21a2f7ca4b93b06e238cf2b9da0af3947c7257

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        erb :index
+    end
+
+    params = {
+        :item => "lunch box"
+    }
+
+    post '/checkout' do
+        "My iem is #{params[:item]}."
+    end
+
 end
